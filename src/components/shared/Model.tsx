ab4f8c756cea3cd98267692e5b20f04b0a41dd7c
Abdullah-142 <abdulah14200@gmail.com>
2024-05-01 11:40:00 -0700

    Refactor form attributes and styles in ContactPage.tsx and Model.tsx, and update newsletter subscription functionality

--- src/components/shared/Model.tsx
+++ src/components/shared/Model.tsx
@@ -6,7 +6,9 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 
+import { subscribeToNewsletter } from "@/lib/requests";
 import { useState } from "react";
+import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 
@@ -16,28 +18,45 @@ type ModelProps = {
    description: string;
    title: string;
    type: "newslettter" | "underMaintenance";
-   handleSubmit?: (email: string) => void;
+   handleSubmit?: () => void;
 };
 
-const Model = ({
-   show,
-   handle,
-   description,
-   title,
-   type,
-   handleSubmit,
-}: ModelProps) => {
+const Model = ({ show, handle, description, title, type }: ModelProps) => {
    const [email, setEmail] = useState<string>("");
+
+   const handleSubmit = async () => {
+      if (!email) {
+         toast.error("Email is required");
+         return;
+      }
+      if (!/\S+@\S+\.\S+/.test(email)) {
+         toast.error("Invalid email format");
+         return;
+      }
+
+      try {
+         const res: any = await subscribeToNewsletter(email);
+         if (res) {
+            toast.success(
+               "Please confirm your email to subscribe to our newsletter"
+            );
+         }
+      } catch (error: any) {
+         if (error.response.errors[0].message === "Email already subscribed") {
+            toast.error("Email already subscribed");
+         }
+      }
+   };
 
    return (
       <Dialog open={show} onOpenChange={handle}>
          <DialogContent>
             <DialogTitle className="text-2xl">{title}</DialogTitle>
-            <div>
+            <div className="space-y-3">
                <p className="max-sm:text-base text-xl">{description}</p>
                {type === "newslettter" && (
-                  <form className="mt-2">
-                     <Label htmlFor="email" className="mt-4">
+                  <form>
+                     <Label htmlFor="email" className="text-lg">
                         Email
                      </Label>
                      <Input
@@ -52,16 +71,17 @@ const Model = ({
                   </form>
                )}
             </div>
-            <DialogClose asChild className="mt-4">
+            <DialogClose asChild className="mt-3">
                {type === "newslettter" ? (
-                  <Button
-                     type="button"
-                     onClick={() => handleSubmit && handleSubmit(email)}
-                     variant="secondary"
-                     className="focus:outline-none focus:ring-0 focus-visible:ring-offset-0 border-primary focus-visible:ring-transparent focus:border-none  focus:border-transparent"
-                  >
-                     Subscribe
-                  </Button>
+                  email && (
+                     <Button
+                        type="button"
+                        onClick={handleSubmit}
+                        className="focus:outline-none focus:ring-0 focus-visible:ring-offset-0 border-primary focus-visible:ring-transparent focus:border-none  focus:border-transparent"
+                     >
+                        Subscribe
+                     </Button>
+                  )
                ) : (
                   <Button
                      type="button"
