efa2ea5fb858cf1d2fc7f065d13e412eaf6cf85b
Abdullah-142 <abdulah14200@gmail.com>
2024-05-01 02:01:06 -0700

    Refactor form attributes and styles in ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -2,24 +2,47 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import Layout from "@/Layout";
+import { useState } from "react";
 
 const ContactPage = () => {
+   const [state, setState] = useState({ email: "", name: "", message: "" });
+   const handleChange = (e: any) =>
+      setState({
+         ...state,
+         [e.target.name]: e.target.value,
+      });
+
+   const encode = (data: any) => {
+      return Object.keys(data)
+         .map(
+            (key) =>
+               encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+         )
+         .join("&");
+   };
+
+   const handleSubmit = (e: any) => {
+      console.log({ ...state });
+      fetch("/", {
+         method: "POST",
+         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+         body: encode({ "form-name": "contact", ...state }),
+      })
+         .then(() => alert("Success!"))
+         .catch((error) => alert(error));
+
+      e.preventDefault();
+   };
    return (
       <Layout>
          <section>
             <div className="px-4 lg:p-0 text-base">
                <form
-                  target="_blank"
                   className="flex flex-col gap-5"
-                  method="post"
                   name="contant-form"
+                  onSubmit={handleSubmit}
                   data-netlify="true"
                >
-                  <input
-                     type="hidden"
-                     name="content-form"
-                     value="contact-form"
-                  />
                   <div>
                      <label
                         htmlFor="email"
@@ -31,6 +54,8 @@ const ContactPage = () => {
                         type="email"
                         id="email"
                         name="email"
+                        value={state.email}
+                        onChange={handleChange}
                         className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                         placeholder="yourname@.com"
                         required
@@ -47,6 +72,8 @@ const ContactPage = () => {
                         type="text"
                         id="Name"
                         name="name"
+                        value={state.name}
+                        onChange={handleChange}
                         className="block p-3 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                         placeholder="Let us know how we can help you"
                         required
@@ -61,6 +88,8 @@ const ContactPage = () => {
                      </label>
                      <Textarea
                         id="message"
+                        value={state.message}
+                        onChange={handleChange}
                         name="message"
                         rows={6}
                         className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:border-primary-500"
