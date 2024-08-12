cfb61f36cfbcf81b82d2378c4eea3cc9ff0e79bf
Abdullah-142 <abdulah14200@gmail.com>
2024-08-12 19:21:52 +0500

    Fox a project bug

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -3,11 +3,14 @@ import { projects } from "@/constants";
 import { Link } from "react-router-dom";
 
 const ProjectsPage = () => {
+	// Create a new reversed array without mutating the original
+	const reversedProjects = [...projects].reverse();
+
 	return (
 		<>
 			<h1 className="text-4xl font-bold">Projects</h1>
 			<div className="flex flex-col mt-8">
-				{projects.reverse().map((project) => (
+				{reversedProjects.map((project) => (
 					<Link key={project.id} to={`${project.slug}`}>
 						<Posts key={project.id} slug={project.slug} description={project.description} />
 					</Link>
