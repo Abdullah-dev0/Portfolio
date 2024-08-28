5bf34fcea2ff64445c3ed2eb9ecc103da49fc97e
Abdullah-142 <abdulah14200@gmail.com>
2024-08-29 03:30:16 +0500

    Update project section to display latest projects based on slice index

--- src/components/shared/ProjectSection.tsx
+++ src/components/shared/ProjectSection.tsx
@@ -4,23 +4,23 @@ import Posts from "./Posts";
 import { Button } from "../ui/button";
 
 const PostSection = () => {
-  return (
-    <section className="w-full mt-12">
-      <div className="flex items-center text-[18px] max-sm:text-[16px] gap-2  justify-between flex-wrap">
-        <h1 className="">Latest Projects</h1>
-        <Link to="/projects">
-          <Button variant={"secondary"}>See All Project</Button>
-        </Link>
-      </div>
-      <div>
-        {projects.slice(0, 2).map((project) => (
-          <Link to={`projects/${project.slug}`} key={project.id}>
-            <Posts key={project.id} slug={project.slug} description={project.description} />
-          </Link>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section className="w-full mt-12">
+			<div className="flex items-center text-[18px] max-sm:text-[16px] gap-2  justify-between flex-wrap">
+				<h1 className="">Latest Projects</h1>
+				<Link to="/projects">
+					<Button variant={"secondary"}>See All Project</Button>
+				</Link>
+			</div>
+			<div>
+				{projects.slice(-2).map((project) => (
+					<Link to={`projects/${project.slug}`} key={project.id}>
+						<Posts key={project.id} slug={project.slug} description={project.description} />
+					</Link>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default PostSection;
