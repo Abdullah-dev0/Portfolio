efe5f6b957dad6511ae71844ad4404505bd62f9c
Abdullah-142 <abdulah14200@gmail.com>
2024-08-28 18:36:45 +0500

    Add pagination for projects Page

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,10 +1,14 @@
 import Posts from "@/components/shared/Posts";
+import { Button } from "@/components/ui/button";
 import { projects } from "@/constants";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const ProjectsPage = () => {
+	const [currentPage, setCurrentPage] = React.useState(1);
+
 	// Create a new reversed array without mutating the original
-	const reversedProjects = [...projects].reverse();
+	const reversedProjects = [...projects].reverse().slice(0, currentPage * 3);
 
 	return (
 		<>
@@ -15,6 +19,22 @@ const ProjectsPage = () => {
 						<Posts key={project.id} slug={project.slug} description={project.description} />
 					</Link>
 				))}
+
+				<div className="text-center">
+					{currentPage * 3 > projects.length ? (
+						<p className="text-center mt-10 text-gray-500">
+							🎉 You've reached the end! No more Projects to show for now. Stay tuned for more amazing Projects! 🚀
+						</p>
+					) : (
+						<Button
+							onClick={() => setCurrentPage((prev) => prev + 1)}
+							variant="ghost"
+							size="lg"
+							className="text-md underline mt-2">
+							Load more
+						</Button>
+					)}
+				</div>
 			</div>
 		</>
 	);
