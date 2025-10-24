c8b5c32b9afa42e01462e945d4e8542aa197e438
Abdullah-dev0 <abdulah14200@gmail.com>
2025-10-25 04:33:25 +0500

    feat: add Cloudflare Web Analytics script and remove Vercel Analytics component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./app.tsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<>
 		<App />
-		<Analytics />
 	</>,
 );
