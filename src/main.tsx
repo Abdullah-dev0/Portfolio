40fa8fc48d26163d765ad60b208cccaf9231f132
Abdullah-dev0 <abdulah14200@gmail.com>
2025-10-25 15:03:25 +0500

    refactor: streamline rendering of App component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,4 @@
 import ReactDOM from "react-dom/client";
 import App from "./app.tsx";
 import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<>
-		<App />
-	</>,
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
